8a89b8ba0d48e4ca95aef74581252fa27c6b9ca5
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-13 17:32:29 +0900

    Create transformers.py

---
+++ transformers.py
@@ -0,0 +1,48 @@
+# Util functions for Huggingface Transformers
+
+from typing import Dict
+from transformers import AutoTokenizer
+
+def load_tokenizer(
+    pretrained_model_name_or_path: str, 
+    special_token_dict: Dict
+):
+    """Load transfomers tokenizer
+    
+    Args:
+        pretrained_model_name_or_path: the `model id` of huggingface hub or 'model path' to the directory where the pretrained tokenizer saved
+        special_token_dict: dictionary which key is speicial token name, and value is speical token
+        
+    Returns:
+        tokenizer: transformers tokenizer
+        
+    Examples:
+        special_token_dict = {
+            'bos_token': '<s>',
+            'eos_token': '</s>',
+            'title_token': '<title>', # additional special token
+        }
+        tokenizer = load_tokenizer('skt/kogpt2-base-v2', special_token_dict)
+        print(tokenizer.title_token) # '<title>'
+    """
+    
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+    
+    predefined_special_tokens = ['bos_token', 'eos_token', 'unk_token', 'sep_token', 'pad_token', 'cls_token', 'mask_token']
+    predefined_special_token_dict = {}
+    additional_special_token_dict = {}
+
+    for k, v in special_token_dict.items():
+        if k in predefined_special_tokens:
+            predefined_special_token_dict[k] = v
+        else:
+            additional_special_token_dict[k] = v
+
+    predefined_special_token_dict['additional_special_tokens'] = list(additional_special_token_dict.values())
+    tokenizer.add_special_tokens(predefined_special_token_dict)
+
+    for k, v in additional_special_token_dict.items():
+        setattr(tokenizer, f'{k}', v)
+        setattr(tokenizer, f'{k}_id', tokenizer.convert_tokens_to_ids(v))
+
+    return tokenizer
