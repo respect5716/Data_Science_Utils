e9e22205e3c08686270fd5bb3f0b490756623918
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-13 20:49:05 +0900

    Update pytorch_lightning.py

--- pytorch_lightning.py
+++ pytorch_lightning.py
@@ -50,3 +50,33 @@ class Module(pl.LightningModule):
     
     def test_step(self, batch, batch_idx):
         return self.step(batch, 'test')
+
+    
+    
+class CheckpointCallback(pl.Callback):
+    def __init__(self, ckpt_dir: str, metric: str = 'valid/loss_epoch', mode: str = 'min'):
+        self.ckpt_dir = ckpt_dir
+        self.metric = metric
+        self.mode = mode
+        self.best_metric = 1e10 if mode == 'min' else -1e10 
+        
+        if not os.path.isdir(ckpt_dir):
+            os.makedirs(ckpt_dir)
+    
+    def should_save(self, curr_metric):
+        if self.mode == 'min':
+            return curr_metric < self.best_metric
+        else:
+            return curr_metric > self.best_metric
+    
+    def save_checkpoint(self, model):
+        raise NotImplementedError("Implement save checkpoint method.")
+    
+    def on_validation_epoch_end(self, trainer, model):
+        curr_metric = trainer.callback_metrics[self.metric]
+        if self.should_save(curr_metric):
+            print(f'STEP: {trainer.global_step:06d} | {self.metric}: {curr_metric:.3f} | model saved ({self.best_metric:.3f} -> {curr_metric:.3f})')
+            self.save_checkpoint(model)
+            self.best_metric = curr_metric
+        else:
+            print(f'STEP: {trainer.global_step:06d} | {self.metric}: {curr_metric} | model not saved (best metric: {self.best_metric:.3f})')
