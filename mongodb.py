0220f71dd05edc191dfa5d2094ec5d80fee65fe0
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-15 19:35:12 +0900

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -9,28 +9,30 @@ def pandas_to_mongo(
     database_name: str,
     collection_name: str,
     columns: Optional[Union[List, Dict]] = None,
+    specify_id: bool = True,
 ) -> None:
-  """Insert pandas dataframe to mongodb collection
+    """Insert pandas dataframe to mongodb collection
   
-  Args:
-    client: mongodb client
-    dataframe: dataframe to insert
-    database_name
-    collection_name
-    columns: columns to be included 
-      - if None, all columns will be included
-      - if List, columns in the list will be included
-      - if Dict, columns in the dict keys will be included as the dict values
-      
-  Examples:
-    client = MongoClient(host='localhost', port=27017)
-    data = pd.read_csv('data.csv')
-    columns = {'col1':'new_col1', 'col3':'new_col3'}
-    pandas_to_mongo(client, data, 'database', 'collection', columns)
+    Args:
+        client: mongodb client
+        dataframe: dataframe to insert
+        database_name
+        collection_name
+        columns: columns to be included 
+          - if None, all columns will be included
+          - if List, columns in the list will be included
+          - if Dict, columns in the dict keys will be included as the dict values
+
+    Examples:
+        client = MongoClient(host='localhost', port=27017)
+        data = pd.read_csv('data.csv')
+        columns = {'col1':'new_col1', 'col3':'new_col3'}
+        pandas_to_mongo(client, data, 'database', 'collection', columns)
     """
     database = client[database_name]
     collection = database[collection_name]
-
+    cnt = collection.count()
+    
     if columns:
         if type(columns) == dict:
             dataframe = dataframe[list(columns.keys())]
@@ -38,6 +40,9 @@ def pandas_to_mongo(
         
         elif type(columns) == list:
             dataframe = dataframe[columns]
+        
+        if specify_id:
+            dataframe['_id'] = [i for i in range(cnt, cnt+len(dataframe))]
 
     documents = dataframe.to_dict('records')
     _ = collection.insert_many(documents)
