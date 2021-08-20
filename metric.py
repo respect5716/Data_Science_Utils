a824384c6e15e7ba84a60e6da45d6945ff40ab30
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-20 21:28:52 +0900

    Create metric.py

---
+++ metric.py
@@ -0,0 +1,6 @@
+def jaccard_similarity(lst1: List, lst2: List):
+    """Jaccard similarity for guaging the similarity and diversity of sample sets
+    """
+    set1 = set(lst1)
+    set2 = set(lst2)
+    return len(set1 & set2) / len(set1 | set2)
