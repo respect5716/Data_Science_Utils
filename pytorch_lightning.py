3a1e83b80970d5beac457f2ebc629909d5f05c10
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-27 14:03:44 +0900

    Update pytorch_lightning.py

--- pytorch_lightning.py
+++ pytorch_lightning.py
@@ -1,21 +1,65 @@
 # util functions for pytorch_lightning
 import os
 import pytorch_lightning as pl
-from typing import List, Callable
+from typing import Optional, Any, Dict, List, Callable
+from transformers import get_scheduler
 
 
+class AttributeDict(pl.utilities.parsing.AttributeDict):
+    def __init__(self, data):
+        for k, v in data.items():
+            self.__setattr__(k, v)
+    
+    def __setattr__(self, key: str, val: Any) -> None:
+        if isinstance(val, dict):
+            val = AttributeDict(val)
+        self[key] = val
+        
+
+OPTIM_DICT = {
+    'sgd': torch.optim.SGD,
+    'adam': torch.optim.Adam,
+    'adamw': torch.optim.AdamW
+}
+
+
+def help_optimizer(optim_name = None):
+    if optim_name in OPTIM_DICT:
+        help(OPTIM_DICT[optim_name])
+    else:
+        print(f'choose optimizer from {list(OPTIM_DICT.keys())}')
+        
+        
+def get_optimizer(optim_name, params, **kwargs):
+    optimizer = OPTIM_DICT.get(optim_name.lower())
+    if optimizer is None:
+        raise ValueError
+        
+    optimizer = optimizer(params, **kwargs)
+    return optimizer
+    
+    
 class Module(pl.LightningModule):
     """Base module for pytorch_lightning
     """
     
-    @property
+    def check_hyperparameters(self, hparams):
+        assert 'trainer' in hparams
+        assert 'optimizer' in hparams
+        
+        
+    def save_hyperparameters(self, hparams):
+        self.check_hyperparameters(hparams)
+        for k, v in hparams.items():
+            self.hparams[k] = v
+    
     def param_groups(self):
         no_decay = ["bias", "bn", "ln", "norm"]
         param_groups = [
             {
                 # apply weight decay
                 "params": [p for n, p in self.named_parameters() if not any(nd in n.lower() for nd in no_decay)],
-                "weight_decay": self.hparams.get('weight_decay', 0.0),
+                "weight_decay": self.hparams.optimizer.params.get('weight_decay', 0.0),
             },
             {
                 # not apply weight decay
@@ -27,21 +71,49 @@ class Module(pl.LightningModule):
         return param_groups
     
     @property
-    def num_training_steps(self):
-        assert 'batch_size' in self.hparams, 'batch size is required in hparams'
-        assert 'epoch_size' in self.hparams, 'epoch size is required in hparams'
+    def num_train_batches(self):
+        num_batches = len(self.train_dataloader())
+
+        if type(self.trainer.limit_train_batches) == float:
+            num_batches = int(num_batches * self.trainer.limit_train_batches)
+        else:
+            num_batches = min(self.trainer.limit_train_batches, num_batches)
         
-        effective_batch_size = self.hparams.batch_size * self.trainer.accumulate_grad_batches * self.trainer.num_gpus
-        num_training_steps = int(len(self.train_dataloader.dataloader.dataset) / effective_batch_size * self.hparams.epoch_size)
+        return num_batches
+    
+    @property
+    def num_devices(self):
+        num_devices = max(1, self.trainer.num_gpus, self.trainer.num_processes)
+        if self.trainer.tpu_cores:
+            num_devices = max(num_devices, self.trainer.tpu_cores)
+        return num_devices
+    
+    @property
+    def num_training_steps(self):
+        if self.trainer.max_steps:
+            return self.trainer.max_steps
+        
+        num_devices = 1 if self.trainer.distributed_backend == 'dp' else self.num_devices
+        effective_accum = self.trainer.accumulate_grad_batches * num_devices     
+        num_training_steps = self.num_train_batches // effective_accum * self.trainer.max_epochs
         return num_training_steps
     
     @property
     def num_warmup_steps(self):
-        warmup_ratio = self.hparams.get('warmup_ratio', 0.)
-        num_warmup_steps = int(self.num_training_steps * warmup_ratio)
+        warmup_size = self.hparams.scheduler.get('warmup_size', 0)
+        num_warmup_steps = int(self.num_training_steps * warmup_size) if type(warmup_size) == float else warmup_size
         return num_warmup_steps
     
-    def step(self, batch):
+    def configure_optimizers(self):
+        optimizer = get_optimizer(self.hparams.optimizer.name, self.param_groups(), **self.hparams.optimizer.params)
+        
+        if 'scheduler' in self.hparams:
+            scheduler = get_scheduler(self.hparams.scheduler.name, optimizer, self.num_warmup_steps, self.num_training_steps)
+            return [optimizer], [scheduler]
+        
+        return optimizer
+    
+    def step(self, batch, mode):
         raise NotImplementedError("Implement the step method.")
     
     def training_step(self, batch, batch_idx):
@@ -52,7 +124,7 @@ class Module(pl.LightningModule):
     
     def test_step(self, batch, batch_idx):
         return self.step(batch, 'test')
-
+    
     
     
 class CheckpointCallback(pl.Callback):
