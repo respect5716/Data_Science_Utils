51d3aa9abd1b956a8ebae9ef820e8b681c3c1663
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-08-27 14:12:57 +0900

    Update module.py

--- pytorch_lightning/module.py
+++ pytorch_lightning/module.py
@@ -2,42 +2,8 @@
 import os
 import pytorch_lightning as pl
 from typing import Optional, Any, Dict, List, Callable
-from transformers import get_scheduler
+from .optim import get_optimizer, get_scheduler
 
-
-class AttributeDict(pl.utilities.parsing.AttributeDict):
-    def __init__(self, data):
-        for k, v in data.items():
-            self.__setattr__(k, v)
-    
-    def __setattr__(self, key: str, val: Any) -> None:
-        if isinstance(val, dict):
-            val = AttributeDict(val)
-        self[key] = val
-        
-
-OPTIM_DICT = {
-    'sgd': torch.optim.SGD,
-    'adam': torch.optim.Adam,
-    'adamw': torch.optim.AdamW
-}
-
-
-def help_optimizer(optim_name = None):
-    if optim_name in OPTIM_DICT:
-        help(OPTIM_DICT[optim_name])
-    else:
-        print(f'choose optimizer from {list(OPTIM_DICT.keys())}')
-        
-        
-def get_optimizer(optim_name, params, **kwargs):
-    optimizer = OPTIM_DICT.get(optim_name.lower())
-    if optimizer is None:
-        raise ValueError
-        
-    optimizer = optimizer(params, **kwargs)
-    return optimizer
-    
     
 class Module(pl.LightningModule):
     """Base module for pytorch_lightning
@@ -125,57 +91,4 @@ class Module(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         return self.step(batch, 'test')
     
-    
-    
-class CheckpointCallback(pl.Callback):
-    def __init__(self, ckpt_dir: str, metric: str = 'valid/loss_epoch', mode: str = 'min'):
-        self.ckpt_dir = ckpt_dir
-        self.metric = metric
-        self.mode = mode
-        self.best_metric = 1e10 if mode == 'min' else -1e10 
-        
-        if not os.path.isdir(ckpt_dir):
-            os.makedirs(ckpt_dir)
-    
-    def should_save(self, curr_metric):
-        if self.mode == 'min':
-            return curr_metric < self.best_metric
-        else:
-            return curr_metric > self.best_metric
-    
-    def save_checkpoint(self, model):
-        raise NotImplementedError("Implement save checkpoint method.")
-    
-    def on_validation_epoch_end(self, trainer, model):
-        curr_metric = float(trainer.callback_metrics[self.metric])
-        if self.should_save(curr_metric):
-            print(f'STEP: {trainer.global_step:06d} | {self.metric}: {curr_metric:.3f} | model saved ({self.best_metric:.3f} -> {curr_metric:.3f})')
-            self.save_checkpoint(model)
-            self.best_metric = curr_metric
-        else:
-            print(f'STEP: {trainer.global_step:06d} | {self.metric}: {curr_metric:.3f} | model not saved (best metric: {self.best_metric:.3f})')
-
-            
-            
-class MessageCallback(pl.Callback):
-    def __init__(self, title: str, message_fn: Callable, metrics: List[str]):
-        self.title = title
-        self.message_fn = message_fn
-        self.metrics = metrics
-
-    def on_train_start(self, trainer, pl_module):
-        self.message_fn(f'{self.title} train started!!')
-
-    def on_train_end(self, trainer, pl_module):
-        self.message_fn(f'{self.title} train ended!!')
-
-    def on_epoch_end(self, trainer, pl_module):
-        message = f'{self.title} log\n'
-        message += f'step: {trainer.global_step:06d}\n'
-        for k in self.metrics:
-            v = trainer.callback_metrics.get(k, 'none')
-            v = v if type(v) == str else f'{v:.3f}'
-            message += f'{k}: {v}\n'
-        
-        message += '=' * 50
-        self.message_fn(message)
+ 
