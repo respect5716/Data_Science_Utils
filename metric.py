138a1bc84c459bb7fbf31ca92350092fd9542c93
윤용선 <65707703+respect5716@users.noreply.github.com>
2021-09-19 12:47:46 +0900

    Update metric.py

--- metric.py
+++ metric.py
@@ -1,3 +1,4 @@
+import torch
 from typing import List
 
 def jaccard_similarity(lst1: List, lst2: List):
@@ -6,3 +7,8 @@ def jaccard_similarity(lst1: List, lst2: List):
     set1 = set(lst1)
     set2 = set(lst2)
     return len(set1 & set2) / len(set1 | set2)
+
+def accuracy(logits: torch.Tensor, labels: torch.Tensor):
+    preds = logits.max(dim=1)[1]
+    acc = (preds == labels).float().mean()
+    return acc
